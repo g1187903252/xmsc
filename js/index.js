7d937aeec7f748a99695d3c00dfca9916805e8b5
g1187903252 <1187903252@qq.com>
2020-11-23 21:29:24 +0800

    主页样式写了75%

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-document.querySelector('h1').onclick = function (){
-  alert(123);
-};
+var mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal', // 切换选项
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  speed: 2000,
+  autoplay: {
+    delay: 5000
+  },
+  loop: true, // 循环模式选项
+
+  pagination :{
+    el: '.swiper-pagination',
+    clickable :true,
+    },
+
+  // 如果需要前进后退按钮
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+})
